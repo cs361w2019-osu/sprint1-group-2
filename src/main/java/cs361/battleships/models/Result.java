d3a3e00e80f853ae20241f4ea7c5e5cfb11af3f3
wilsconn <wilsconn@oregonstate.edu>
2019-01-22 16:35:47 -0800

    removed uneccesary comments

--- src/main/java/cs361/battleships/models/Result.java
+++ src/main/java/cs361/battleships/models/Result.java
@@ -10,38 +10,29 @@ public class Result {
 	private Ship ship;
 
 	public AtackStatus getResult() {
-		//TODO implement
 		return this.result;
-		//return null;
 	}
 
 	public void setResult(AtackStatus result) {
-		//TODO implement
 		this.result = result;
 	}
 
 	public Ship getShip() {
-		//TODO implement
 		return this.ship;
-		//return null;
 	}
 
 	public void setShip(Ship ship) {
 
-		//TODO implement
 		this.ship = ship;
 
 	}
 
 	public Square getLocation() {
-		//TODO implement
 
 		return this.location;
-		//return null;
 	}
 
 	public void setLocation(Square square) {
-		//TODO implement
 		this.location = square;
 	}
 }
