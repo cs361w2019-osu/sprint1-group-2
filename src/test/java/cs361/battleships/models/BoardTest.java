1d195f24b30aa174e96171e260231e4922e7ee68
racestewart <35312415+racestewart@users.noreply.github.com>
2019-01-22 20:26:45 -0800

    Update BoardTest.java

--- src/test/java/cs361/battleships/models/BoardTest.java
+++ src/test/java/cs361/battleships/models/BoardTest.java
@@ -19,11 +19,6 @@ public class BoardTest {
         assertFalse(board.placeShip(new Ship("MINESWEEPER"), 11, 'D', false));
         assertFalse(board.placeShip(new Ship("MINESWEEPER"), 11, 'I', true));
         
-        assertTrue(board.attack(1, 'A'));
-        assertTrue(board.attack(3, 'C'));
-        assertTrue(board.attack(7, 'I'));
-
-        assertFalse(board.attack(6, 'A'));
-        assertFalse(board.attack(7, 'A'));
+        assertEquals('HIT', board.attack(1, 'A').getResult());
     }
 }
