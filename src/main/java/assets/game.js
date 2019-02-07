ed4982a48ecf17deaef7917220e05dfce59a2e85
Hunter Lannon <kindlehl@osuosl.org>
2019-02-06 23:04:31 -0800

    changed message on bad placement

--- src/main/java/assets/game.js
+++ src/main/java/assets/game.js
@@ -87,7 +87,7 @@ function sendXhr(method, url, data, handler) {
     var req = new XMLHttpRequest();
     req.addEventListener("load", function(event) {
         if (req.status != 200) {
-            alert("Cannot complete the action");
+            alert("Two ships may not occupy one square. Please try placing in another spot.");
             return;
         }
         handler(JSON.parse(req.responseText));
